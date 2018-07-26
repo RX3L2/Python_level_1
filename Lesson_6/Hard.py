4fe32c69b83f609e6196223621982afdfe7a8f07
rx3l2 <ivan9991@yandex.ru>
2018-07-26 10:38:20 +0700

    ДЗ повышенной сложности, добавлено задание -2

--- Lesson_6/Hard.py
+++ Lesson_6/Hard.py
@@ -48,3 +48,33 @@ print(toys_1.name, toys_1.colour, toys_1.type)
 # исходя из типа игрушки отдавать конкретный объект класса, который наследуется от базового - Игрушка
 
 
+class Animal_toy(Toy):
+    def __init__(self, name, colour):
+        type = 'животное'
+        super().__init__(name, colour, type)
+
+
+class Mult_persona_toy(Toy):
+    def __init__(self, name, colour):
+        type = 'персонаж мультфильма'
+        super().__init__(name, colour, type)
+
+class Factory_toys_new(Factory_toys):
+    def __init__(self, name, colour, type):
+        super().__init__(name, colour, type)
+
+    def factory_run(self):
+        self.purchase()
+        self.sewing()
+        self.colouring()
+
+        return Animal_toy(self.name, self.colour) if self.type == 'Animal' else Mult_persona_toy(self.name, self.colour)
+
+print('----- Задача - 2 ------')
+factory2 = Factory_toys_new('Крокодил', 'зеленый', 'Animal')
+toys_2 = factory2.factory_run()
+print(toys_2.name, toys_2.colour, toys_2.type)
+
+factory3 = Factory_toys_new('Смешарик', 'синий', 'Mult')
+toys_3 = factory3.factory_run()
+print(toys_3.name, toys_3.colour, toys_3.type)
